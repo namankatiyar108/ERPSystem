6c4a13ccf008a95f2104ad08dee7a70cffa48c95
Naman Katiyar <namankatiyar111@gmail.com>
2024-11-18 15:59:07 +0530

    write the signup and sign in controller

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import {DB_NAME} from '../constants.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path:'./.env',
+});
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,{useUnifiedTopology: true, useNewUrlParser: true,})
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
--- Backend/src/middlewears/auth.middlewear.js
+++ Backend/src/middlewears/auth.middlewear.js
@@ -2,7 +2,61 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.models.js";
- 
+import bcryptjs from bcryptjs;
+
+export const signup = async(req, res, next) => {
+    const {username, email, password,} = req.body;
+
+    if(!username || !email || !password || username === '' || email === '' || password === ''){
+        next(ApiError(400, 'All Fields are required'));
+    }
+    const hashedPassword = bcryptjs.hashSync(password,10);
+
+// new User is method which will automatically detect if user is present or not through user model
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    try {
+        await newUser.save();
+        res.json("User signup succesfully")
+        
+    } catch (error) {
+        next(ApiError(400, "User with username or email Already registered"))
+        
+    }
+}
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    if(!email || !password || email === "" || password === ""){
+        return next(ApiError(400, "All field are reuired"));
+    }
+    try {
+        const validuser = await User.findOne({email});
+        if(!validuser){
+            return next(ApiError(400,"Email id is not valid"))
+        }
+
+        const validpassword = bcryptjs.compareSync(password, validuser.password)
+        if(!validpassword){
+            return next(ApiError(400, "Please enter correct password"));
+
+        }
+        const token = jwt.sign({id : validuser._id, isAdmin: validuser.isAdmin}, process.env.ACCESS_TOKEN_SECRET);
+
+        const {password: pass, ...rest} = validuser._doc //it is used to sepatreate passwor and other details
+        res.status(200).cookies('access_token', token, { httpOnly: true }).json(rest);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+
+}
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
